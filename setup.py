2de2795cd9cef35078109a4c561907ccdb6eee42
Blake Justice <13934264+bljustice@users.noreply.github.com>
2020-06-20 09:58:11 -0500

    Fix requirements and setup

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from ynabdashetl import __version__
 from setuptools import setup, find_packages
 
 setup(
-    name='YNAB Dash ETL',
+    name='ynab-dash-etl',
     version=__version__,
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
     zip_safe=False,
-    install_requires=['ynab-sdk-python<1'],
+    install_requires=[
+        'ynab-sdk-python@git+https://github.com/bljustice/ynab-sdk-python.git@master'
+    ],
     python_requires='>=3'
 )
