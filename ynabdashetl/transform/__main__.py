96fdb9683c5ee531be39baf999815b005c802ab7
Blake Justice <13934264+bljustice@users.noreply.github.com>
2022-08-07 14:51:13 -0500

    Build new source layer

--- ynabdashetl/transform/__main__.py
+++ ynabdashetl/transform/__main__.py
@@ -1,6 +1,12 @@
+import argparse
+
 from ynabdashetl.transform import BudgetTransformJob
 
 if __name__ == '__main__':
     
-    job = BudgetTransformJob()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--start_date", help="start date of data to load", type=str)
+    args = parser.parse_args()
+
+    job = BudgetTransformJob(args.start_date)
     job.run()
