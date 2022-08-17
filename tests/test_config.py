39d56543e54f3108adf261f7b45eb648b1c2cf98
Blake Justice <13934264+bljustice@users.noreply.github.com>
2022-08-17 12:47:43 -0500

    Rewrite config tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,13 +1,19 @@
+import os
 import unittest
 
-from ynabdashetl.extract.config import build_config
+from ynabdashetl.core.config import build_config
 
 
 class ConfigTests(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        """"
+        """
+        cls.config = build_config('development')
+
     def test_build_config(self):
  
-        env = 'development'
-        result = build_config(env)
-        self.assertIn('ENV', result)
-        self.assertIsInstance(result, dict)
+        self.assertIn('YNAB_PERSONAL_TOKEN', dir(self.config))
+        self.assertIn('YNAB_BUDGET_ID', dir(self.config))
+        self.assertEqual(self.config.DEBUG, True)
